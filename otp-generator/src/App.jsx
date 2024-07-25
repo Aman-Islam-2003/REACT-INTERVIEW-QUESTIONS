dfcb6509b0eff9d8893736c486fae8f08053d33e
Aman-Islam-2003 <amanislam019@gmail.com>
2024-07-25 23:51:19 +0500

    otp-generator

---
+++ otp-generator/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PhoneOtpForm from './components/PhoneOtpForm'
+import GridLights from './components/GridLights'
+GridLights
+
+const App = () => {
+  return (
+    <div>
+      {/* <PhoneOtpForm/> */}
+      <GridLights/>
+    </div>
+  )
+}
+
+export default App
