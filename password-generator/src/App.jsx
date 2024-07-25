95009b0de38e12edc473738a4202e2ab3cfbb85a
Aman-Islam-2003 <amanislam019@gmail.com>
2024-07-25 23:52:07 +0500

    basic configuration

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import ProgressBar from './components/ProgressBar'
+import LikeButton from './components/LikeButton'
 
 
 function App(){
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
 
-  useEffect(()=>{
-    setInterval(()=>{
-       setValue((value)=>(
-        value+1
-       ))
-    },100)
+  // useEffect(()=>{
+  //   setInterval(()=>{
+  //      setValue((value)=>(
+  //       value+1
+  //      ))
+  //   },100)
 
-  },[])
+  // },[])
   return(
     <>
-    <ProgressBar value={value}/>
+    <LikeButton/>
+    {/* <ProgressBar value={value}/> */}
     
     </>
   )
