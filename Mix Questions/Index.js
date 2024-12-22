c697502b018333c2abc0f30c984141fdaaebe4c4
Aman Islam <amanislam019@gmail.com>
2024-12-22 23:40:54 +0500

    Update Index.js

--- Mix Questions/Index.js
+++ Mix Questions/Index.js
@@ -15,7 +15,7 @@ mixArray.forEach((item) => {
 console.log(numbers);
 console.log(characters);
 
-// console.log(mixArray.map(item=>typeof(item)))
+console.log(mixArray.map(item=>typeof(item)))
 
 // Program 2 ->> Function currying
 
@@ -51,4 +51,44 @@ console.log(reduceArray) // return an array
 const sumResult = reduceArray.reduce((accumulator, currentValue) => { return accumulator + currentValue}, 0);
 console.log(sumResult)
 
+//Map vs forEach
+//map wont change the original array but foreach will change the original array
 
+const arr = [1,2,3,4,5,6];
+const newArr =  arr.map((item)=>item+2)
+console.log("map", newArr);
+console.log("original", arr)
+
+const forEachArray = arr.forEach((item)=>item+2);
+console.log(forEachArray,"-------",arr)
+
+const forEachArray2 = arr.forEach((item, index)=>arr[index]= item+2);
+console.log(forEachArray2,"-------",arr)
+
+//null vs undefined
+// null is an actual value where as undefined is that variable is declared but not initialized
+console.log(typeof(null), typeof(undefined))
+let a;
+console.log(a)
+a=null;
+console.log(a)
+
+//event delegation --> adding event listener to parent element and then checking the target element
+
+//flattening the array
+let arrFlattening =[[1,2], [3,4], [5,6], [7, [8,9], 10]];
+console.log(arrFlattening.flat(1))
+
+function customFlattening(arr, depth = 1) {
+  let result = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item) && depth > 0) {
+      result.push(...customFlattening(item, depth - 1));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+console.log(customFlattening([[1, 2], [3, 4], [5, 6], [7, [8, 9], 10]], 2));
