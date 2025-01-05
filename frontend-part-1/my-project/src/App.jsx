b39e3fd0eb238985f0a44b99155e51d9442c72a2
Aman Islam <amanislam019@gmail.com>
2025-01-05 19:05:19 +0500

    Update App.jsx

--- frontend-part-1/my-project/src/App.jsx
+++ frontend-part-1/my-project/src/App.jsx
@@ -1,11 +1,40 @@
-import FileExplorer from "./components/File-Explorer"
-import { explorer } from "./data"
+import { useEffect, useReducer } from "react";
+import FileExplorer from "./components/File-Explorer";
+import { explorer } from "./data";
+import axios from "axios";
+import { cartReducer } from "./reducers/cartReducer";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+    cart: [],
+  });
+
+  console.log(state)
+  const fetchProducts = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+    // console.log(data.products);
+    dispatch({
+      type: 'ADD_PRODUCTS',
+      payload: data.products
+    })
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
-   <FileExplorer explorer={explorer}/>
-  )
+    <div style={{
+      display: "flex"
+    }}>
+      {/* <FileExplorer explorer={explorer} /> */}
+      <Products state={state} dispatch={dispatch} />
+      <Cart state={state} dispatch={dispatch} />
+    </div>
+  );
 }
 
-export default App
+export default App;
