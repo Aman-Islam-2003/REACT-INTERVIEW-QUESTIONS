641bf8bbf6fa7630e1955f7ee2357880e347b12b
Aman Islam <amanislam019@gmail.com>
2025-01-27 23:43:28 +0500

    Delete globalExecutionContext.js

--- Promises/globalExecutionContext.js
+++
@@ -1,9 +0,0 @@
-// whenever a js code runs GEC is created 
-//2 phases
-//memory and code/thread phase
-
-// traverses the whole code and all the variables are loaded in memory
-//initial value is undefined
-
-//Code phase executes line by line
-//every ftn in js has its own local conte  xt
