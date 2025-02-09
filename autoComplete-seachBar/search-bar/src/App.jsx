e0402acb65cbe9e005b9baba5a08d77a3f916149
Aman Islam <amanislam019@gmail.com>
2025-02-09 23:27:50 +0500

    Update App.jsx

--- autoComplete-seachBar/search-bar/src/App.jsx
+++ autoComplete-seachBar/search-bar/src/App.jsx
@@ -1,50 +1,50 @@
-import { useEffect } from "react";
-import { useState } from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [input, setInput] = useState("");
-  const [showResults, setShowResults] = useState(false);
+// function App() {
+//   const [recipes, setRecipes] = useState([]);
+//   const [input, setInput] = useState("");
+//   const [showResults, setShowResults] = useState(false);
 
-  const fetchData = async () => {
-    const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
-    const result = await data.json();
-    setRecipes(result?.recipes);
-  };
+//   const fetchData = async () => {
+//     const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
+//     const result = await data.json();
+//     setRecipes(result?.recipes);
+//   };
 
-  useEffect(() => {
-    fetchData();
-  }, [input]);
+//   useEffect(() => {
+//     fetchData();
+//   }, [input]);
 
-  return (
-    <div>
-      <h1 className="font-bold text-center text-2xl my-5">
-        Autocomplete Search Bar
-      </h1>
-      <div className="border rounded-lg w-1/2 mx-auto p-2">
-        <input
-          type="text"
-          placeholder="search"
-          className="outline-none"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onFocus={()=>setShowResults(true)}
-        />
-      </div>
-      {showResults && (
-        <div className="w-1/2 mx-auto border border-black rounded-lg">
-          {recipes?.map((r) => (
-            <span
-              key={r?.id}
-              className="flex flex-col p-1 hover:cursor-pointer hover:bg-gray-200 hover:rounded-lg"
-            >
-              {r?.name}
-            </span>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1 className="font-bold text-center text-2xl my-5">
+//         Autocomplete Search Bar
+//       </h1>
+//       <div className="border rounded-lg w-1/2 mx-auto p-2">
+//         <input
+//           type="text"
+//           placeholder="search"
+//           className="outline-none"
+//           value={input}
+//           onChange={(e) => setInput(e.target.value)}
+//           onFocus={()=>setShowResults(true)}
+//         />
+//       </div>
+//       {showResults && (
+//         <div className="w-1/2 mx-auto border border-black rounded-lg">
+//           {recipes?.map((r) => (
+//             <span
+//               key={r?.id}
+//               className="flex flex-col p-1 hover:cursor-pointer hover:bg-gray-200 hover:rounded-lg"
+//             >
+//               {r?.name}
+//             </span>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
